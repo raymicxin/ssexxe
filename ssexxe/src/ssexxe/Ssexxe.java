28f37cd881879c72eccafc68fabf60b952264950
xiiiiiiiiin <xiiiiiiiiin@dhcp-25-44.xlate.eduroam.ucl.ac.uk>
2014-07-11 14:14:28 +0100

    xin second commit

--- ssexxe/src/ssexxe/Ssexxe.java
+++ ssexxe/src/ssexxe/Ssexxe.java
@@ -14,6 +14,6 @@ public class Ssexxe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int iV4;// TODO code application logic here
+        int im4;// TODO code application logic here
     }
 }
