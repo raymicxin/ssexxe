2ecea3d3d8d3dd0206294c39a873f3148da3b4fa
Raymond <rezeoma@hotmail.com>
2014-07-11 13:19:19 +0100

    commit 1

--- ssexxe/src/ssexxe/Ssexxe.java
+++ ssexxe/src/ssexxe/Ssexxe.java
@@ -14,6 +14,6 @@ public class Ssexxe {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int i;// TODO code application logic here
     }
 }
